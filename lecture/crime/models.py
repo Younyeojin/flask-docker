e1dd3b44264adcfd59582a261fedba74b9b1ddcb
Younyeojin <yjyoon2107@gmail.com>
2021-10-08 17:57:51 +0900

    1008

---
+++ lecture/crime/models.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+class Crime(object):
+
+    def exec(self):
+        df = pd.read_csv('./data/police_positions.csv.csv', encoding='UTF-8', thousands=',')
+        # 발생 합치기
+        df['범죄 발생'] = df.loc[[''],[2, 5, ]]
+        print(df.info())
+
+if __name__ == '__main__':
+    c = Crime()
+    c.exec()
